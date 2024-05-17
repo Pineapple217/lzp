76f766a966e0b397b805ec432fb6dd43f975208e
Pineapple217 <rosierjules@gmail.com>
2024-05-17 18:21:05 +0200

    added modules

--- lzp.c
+++ lzp.c
@@ -29,6 +29,7 @@ long eval_op(long x, char* op, long y) {
     if(strcmp(op, "-") == 0) { return x - y; }
     if(strcmp(op, "*") == 0) { return x * y; }
     if(strcmp(op, "/") == 0) { return x / y; }
+    if(strcmp(op, "%") == 0) { return x % y; }
 }
 
 long eval(mpc_ast_t* t) {
@@ -57,7 +58,7 @@ int main(int argc, char** argv) {
     mpca_lang(MPCA_LANG_DEFAULT,
     "                                                         \
         number:     /-?[0-9]+/ ;                              \
-        operator:   '+' | '-' | '*' | '/' ;                   \
+        operator:   '+' | '-' | '*' | '/' | '%' ;             \
         expr:       <number> | '(' <operator> <expr>+ ')' ;   \
         lzp:        /^/ <operator> <expr>+ /$/  ;             \
     ",  Number, Operator, Expr, Lzp);
